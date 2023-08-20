588a8f36230b3e885a55fbb86df60e1073748dba
Ganesh Sasidhar Kanigolla <134802322+SasidharKanigolla@users.noreply.github.com>
2023-08-20 10:10:42 +0530

    Time: 1 ms (84.20%), Space: 40.7 MB (46.87%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,19 @@
+class Solution {
+      public boolean backspaceCompare(String S, String T) {
+        return sb(S).equals(sb(T));
+    }
+    
+    private String sb(String str) {
+        StringBuilder sbr = new StringBuilder();
+        
+        for (char c : str.toCharArray()) {
+            
+            if (c != '#') {
+                sbr.append(c);
+            } else if (sbr.length() != 0) {
+                sbr.deleteCharAt(sbr.length() - 1);
+            }
+        }
+        return sbr.toString();
+    }
+}
